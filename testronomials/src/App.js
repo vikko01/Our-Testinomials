5dc597e9b039ea34ef35ca69ae0827bd9f7489a5
vipul sharma <vipulsharmamtr@gmail.com>
2024-10-16 13:35:08 +0530

    our testinomials

---
+++ testronomials/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import Card from './components/Card';
+import reviews from "./data";
+import Testimonials from './components/Testimonials';
+function App() {
+  return (
+    <div className="flex-col flex w-[100vw] h-[100vh] justify-center items-center bg-gray-200" >
+      <div className="text-center">
+
+        <h1 className="text-4xl font-bold">Our Testimonials</h1>
+
+        <div className="bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto"></div>
+
+        <Testimonials reviews={reviews}/>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ testronomials/src/data.js
@@ -0,0 +1,52 @@
+const reviews = [
+    {
+        id: 1,
+        name: "Vipul Sharma",
+        job: "Software Engineer",
+        image: "https://media.istockphoto.com/id/1392990621/photo/smart-handsome-positive-indian-or-arabian-guy-with-glasses-in-casual-wear-student-or.jpg?s=1024x1024&w=is&k=20&c=xA8Vu2F4Uod2OIUM3qGZfoZ7MzCCL3JhYCNfArSwFEY=",
+        text: "A software engineer is a professional who designs, develops, tests, and maintains software applications using scientific and mathematical principles. They work in a variety of industries, including government, healthcare, and telecommunications."
+    },
+
+    {
+        id: 2,
+        name: "Ankit Sharma",
+        job: "Business Man",
+        image: "https://pax.org/images/Vato-FLEX_Spotlight_April_2021_-_800x450.jpg",
+        text:"Businessman is a 2012 Indian Telugu-language action crime film directed and written by Puri Jagannadh and produced by R. R. Venkat under the banner R. R. Movie Makers. It is based from a concept by Ram Gopal Varma and stars an ensemble cast featuring Mahesh Babu, Kajal Aggarwal, Prakash Raj, Nassar, Sayaji Shinde, Raza Murad, Subbaraju and Brahmaji."
+    },
+
+    {
+        id: 3,
+        name: "Megha Sundarani",
+        job: "Associate Software Engineer",
+        image: "https://pbs.twimg.com/profile_images/1087449724783812611/6cqiEHCV_400x400.jpg",
+        text:"Work as part of a team to deliver software solutions. Hands-on experience with construction of complex engineering solutions and knowledge of software development methodologies. Partner with product, infrastructure, DevOps, architecture, and engineering to guide an initiative's technical direction."
+    },
+
+    {
+        id: 4,
+        name: "Kratika Sharma",
+        job: "Make-up Artist",
+        image: "https://media.istockphoto.com/id/1301397300/photo/portrait-of-young-woman-stock-photo.jpg?s=1024x1024&w=is&k=20&c=-q3D6DZVNFtlbaVEGDtGFU907PSZTBHkUlxBzvCSvX0=",
+        text:"Makeup artists apply makeup, and use cosmetic techniques to change or enhance the appearance of their clients for special occasions, such as parties and weddings. These individuals use various application techniques, and skills to create special and long-lasting looks according to the client's requirements."
+    },
+
+    {
+        id: 5,
+        name: "Nikita Sharma",
+        job: "Teacher",
+        image: "https://media.istockphoto.com/id/1369754239/photo/university-student-in-white-background-stock-photo.jpg?s=1024x1024&w=is&k=20&c=yG6Y1qfTygTpg-LH1E54dT0iMn5icY6lHZkUAdwEjWE=",
+        text: "A teacher is a beautiful gift given by god because god is a creator of the whole world and a teacher is a creator of a whole nation. A teacher is such an important creature in the life of a student, who through his knowledge, patience and love give a strong shape to a student's whole life."
+    },
+
+    {
+        id: 6,
+        name: "Mansi Sharma",
+        job: "Student",
+        image: "https://media.istockphoto.com/id/1473711199/photo/face-portrait-student-and-man-in-university-ready-for-back-to-school-learning-goals-or.jpg?s=1024x1024&w=is&k=20&c=1EPVeG3gnxNIMfyc9bfcKZEvyv10SA6T0eB1uLRmwm8=",
+        text: "Writing about the characteristics of an ideal student is a common topic. To write an ideal student paragraph, you can write the attributes of a perfect student in your own words. While writing a paragraph, it is essential to portray your point of view on the topic."
+    },
+];
+
+export default reviews;
+
